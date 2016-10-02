85e59bd35b460f7406db4a259cb6855ce3f12aba
mikerr <mike@redrobe.net>
2016-10-02 22:11:15 +0000

    updating

--- pair.sh
+++ pair.sh
@@ -1,3 +1,6 @@
-bluetoothctl </dev/null
+bluetoothctl </dev/null | grep NexDock > NexDock
+MAC=$(cut -d " " -f 4 < NexDock)
+echo @reboot echo \“connect $MAC \” \| bluetoothctl 
+echo "copy the above and paste into crontab"
+read
 crontab -e 
-@reboot echo “connect MACADDRESSOFNEXDOCK” | bluetoothctl
